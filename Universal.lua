6b3ecce33753a1b1334d6570c4db9b5e981926ea
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-03-14 16:45:05 +0200

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -6394,7 +6394,7 @@ runFunction(function()
 				ServerHop.ToggleButton()
 					local servers = {}
 					local req = httprequest({Url = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true", PlaceId)})
-					local body = HttpService:JSONDecode(req.Body)
+					local body = HttpService:JSONDecode(req.Body end)
 					if body and body.data then
 						for i, v in next, body.data do
 							if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= JobId then
