dc553329e608cda592697871c79036b3fa512eaa
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-03-18 19:08:07 +0200

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -11596,23 +11596,44 @@ runFunction(function()
 		end
 	})
 end)
+
 local function vapeGithubRequest(scripturl)
-	if not isfolder("vape/Profiles") then
-		makefolder("vape/Profiles")
-	else
-		continue
-	end
-	if not isfile("vape/Profiles"..scripturl) then
+	if not isfile("vape/"..scripturl) then
 		local suc, res
 		suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/Erchobg/vapevoidware/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
 		if not suc or res == "404: Not Found" then
-			displayErrorPopup("Failed to connect to github : vape/"..scripturl.." : "..res)
 			error(res)
 		end
 		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
 		writefile("vape/"..scripturl, res)
 	end
 	return readfile("vape/"..scripturl)
+end
+
+local function downloadVapeProfile(path)
+		if not isfile(path) then
+			task.spawn(function()
+				local textlabel = Instance.new("TextLabel")
+				textlabel.Size = UDim2.new(1, 0, 0, 36)
+				textlabel.Text = "Downloading "..path
+				textlabel.BackgroundTransparency = 1
+				textlabel.TextStrokeTransparency = 0
+				textlabel.TextSize = 30
+				textlabel.Font = Enum.Font.SourceSans
+				textlabel.TextColor3 = Color3.new(1, 1, 1)
+				textlabel.Position = UDim2.new(0, 0, 0, -36)
+				textlabel.Parent = GuiLibrary.MainGui
+				repeat task.wait() until isfile(path)
+				textlabel:Destroy()
+			end)
+			local suc, req = pcall(function() return vapeGithubRequest(path:gsub("vape/Profiles", "Profiles")) end)
+			if suc and req then
+				writefile(path, req)
+			else
+				return ""
+			end
+		end
+	return 
 end
 
 runFunction(function()
@@ -11622,7 +11643,23 @@ runFunction(function()
 		HoverText = "Downloads profiles",
 		Function = function(callback) 
 			if callback then
-				
+				local guiprofiles = {}
+    			local profilesfetched
+				task.spawn(function()
+					local res = game:HttpGet('https://api.github.com/repos/Erchobg/vapevoidware/contents/Profiles')
+					if res ~= '404: Not Found' then 
+						for i,v in next, httpservice:JSONDecode(res) do 
+							if type(v) == 'table' and v.name then 
+								table.insert(guiprofiles, v.name) 
+							end
+						end
+					end
+					profilesfetched = true
+				end)
+				repeat task.wait() until profilesfetched
+				for i,v in next, guiprofiles do
+					downloadVapeProfile("vape/Profiles/" v)
+				end
 			end
 		end
 	})
