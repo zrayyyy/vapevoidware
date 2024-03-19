36d9d996182fbde6595d4f52ddfb38f3f0ed8fa5
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-03-19 17:03:08 +0200

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -649,16 +649,20 @@ function VoidwareFunctions:RefreshWhitelist()
 		print(whitelist[i])
 		for i2, v2 in pairs(whitelist[i]) do
 			print(whitelist[i][i2])
-			if whitelist[i][i2].Type == "table" then
-				for i3, v3 in pairs(whitelist[i][i2]) do
-					if whitelist[i][i2][3].Type == "table" then
-						print(whitelist[i][i2][i3])
-					else
-						continue
-					end
-				end
-			else
+			if whitelist[i][i2] == true then
 				continue
+			else
+				if whitelist[i][i2].Type == "table" then
+					for i3, v3 in pairs(whitelist[i][i2]) do
+						if whitelist[i][i2][3].Type == "table" then
+							print(whitelist[i][i2][i3])
+						else
+							continue
+						end
+					end
+				else
+					continue
+				end
 			end
 		end
 	end
