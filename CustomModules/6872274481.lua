a191bfddc25bf2195f69c47eff77072f9ad1a8bc
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-03-21 17:31:49 +0200

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -1,6 +1,17 @@
 local GuiLibrary = shared.GuiLibrary
 local VoidwareFunctions = {WhitelistLoaded = false, WhitelistRefreshEvent = Instance.new("BindableEvent"), WhitelistSucceeded = false, WhitelistLoadTime = tick()}
 local VoidwareLibraries = {}
+local function vapeGithubRequest(scripturl)
+	if not isfile("vape/"..scripturl) then
+		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/Erchobg/vapevoidware/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
+		assert(suc, res)
+		assert(res ~= "404: Not Found", res)
+		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
+		writefile("vape/"..scripturl, res)
+	end
+	return readfile("vape/"..scripturl)
+end
+local RenderFunctions = loadstring(vapeGithubRequest("Libraries/voidwarefunctions.lua"))()
 local VoidwareWhitelistStore = {
 	Hash = "voidwaremoment",
 	BlacklistTable = {},
@@ -178,17 +189,6 @@ local worldtoviewportpoint = function(pos)
 		return scr[1], scr[1].Z > 0
 	end
 	return gameCamera.WorldToViewportPoint(gameCamera, pos)
-end
-
-local function vapeGithubRequest(scripturl)
-	if not isfile("vape/"..scripturl) then
-		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/Erchobg/vapevoidware/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
-		assert(suc, res)
-		assert(res ~= "404: Not Found", res)
-		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
-		writefile("vape/"..scripturl, res)
-	end
-	return readfile("vape/"..scripturl)
 end
 
 local function downloadVapeAsset(path)
