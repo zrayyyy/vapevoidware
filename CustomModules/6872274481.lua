b50ce7949fe18b89ad43e90360a66457e697bf70
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-03-21 18:55:30 +0200

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -11036,6 +11036,11 @@ end)
 
 local function GetAllQueueDescriptions(data)
 	local queuedata, queueids = {}, {}
+	if lprl:GetAttribute("PlayerLevel") > 10 then
+		lplr:SetAttribute("PlayerLevel", 2)
+	else 
+		continue
+	end
 	local rankedunlocked = lplr:GetAttribute("PlayerLevel") and tonumber(lplr:GetAttribute("PlayerLevel")) >= 10 and tonumer(lplr.AccountAge) >= 7
 	for i,v in pairs(bedwars.QueueMeta) do 
 		if v.rankCategory and not rankedunlocked then
