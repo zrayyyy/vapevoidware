b5175ce1f39d2779f450d474a7e8f2d5c1a76e4c
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-03-23 19:55:14 +0200

    Update installer.lua

--- Libraries/installer.lua
+++ Libraries/installer.lua
@@ -1,12 +1,28 @@
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
+local guiprofiles = {}
+local profilesfetched
+task.spawn(function()
+    local res = game:HttpGet('https://api.github.com/repos/Erchobg/vapevoidware/contents/Profiles')
+    if res ~= '404: Not Found' then 
+        for i,v in next, httpservice:JSONDecode(res) do 
+            if type(v) == 'table' and v.name then 
+                table.insert(guiprofiles, v.name) 
+            end
+        end
+    end
+    profilesfetched = true
+    print("step 1 done")
+end)
+
+repeat task.wait() until profilesfetched
+
+task.spawn(function()
+    for i,v in next, guiprofiles do 
+        local res = game:HttpGet('https://raw.githubusercontent.com/Erchobg/vapevoidware/main/Profiles/'..v)
+        task.wait()
+        if res ~= '404: Not Found' then 
+            writevapefile('Profiles/'..v, res) 
+        end
+    end
+end)
+
+return shared.profilesinstalled == true
