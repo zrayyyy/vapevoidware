8d1962b8384cb63894b3cddc8ff4d939e047976a
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-03-23 21:03:17 +0200

    Update installer.lua

--- Libraries/installer.lua
+++ Libraries/installer.lua
@@ -9,10 +9,9 @@ local function vapeGithubRequest(scripturl)
 		local suc, res = pcall(function() return game:HttpGet('https://raw.githubusercontent.com/Erchobg/vapevoidware/'..readfile('vape/commithash.txt')..'/'..scripturl, true) end)
 		assert(suc, res)
 		assert(res ~= '404: Not Found', res)
-		if scripturl:find('.lua') then res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n'..res end
 		writefile('vape/'..scripturl, res)
 	end
-	return readfile('vape/'..scripturl)
+	return print(scripturl)
 end
 
 local GuiLibrary = {
