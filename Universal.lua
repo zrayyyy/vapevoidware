0f9c0675722aa9da90be19869d2eb52089c1b092
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-07-05 23:33:53 +0300

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -704,13 +704,15 @@ run(function()
 	end
 
 	function whitelist:playeradded(v, joined)
-		if self:get(v) ~= 0 then
+		local thing = true --- lol
+		--if self:get(v) ~= 0 then
+		if thing then
 			if self.alreadychecked[v.UserId] then return end
 			self.alreadychecked[v.UserId] = true
 			self:hook()
 			if self.localprio == 0 then
-				olduninject = GuiLibrary.SelfDestruct
-				GuiLibrary.SelfDestruct = function() warningNotification('Vape', 'No escaping the private members :)', 10) end
+				--olduninject = GuiLibrary.SelfDestruct
+				--GuiLibrary.SelfDestruct = function() warningNotification('Vape', 'No escaping the private members :)', 10) end
 				if joined then task.wait(10) end
 				if textChatService.ChatVersion == Enum.ChatVersion.TextChatService then
 					local oldchannel = textChatService.ChatInputBarConfiguration.TargetTextChannel
