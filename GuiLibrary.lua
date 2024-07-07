fbd6822114929172125f40783d79b5f319ba3272
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-07-07 19:08:53 +0300

    Update GuiLibrary.lua

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -1,5 +1,5 @@
 if shared.VapeExecuted then
-	local discord_code = "8Z9kxNWd"
+	local discord_code = "XYaRRwFt"
 	local VERSION = "4.10"..(shared.VapePrivate and " PRIVATE" or "").." "..readfile("vape/commithash.txt"):sub(1, 6)
 	local baseDirectory = (shared.VapePrivate and "vapeprivate/" or "vape/")
 	local vapeAssetTable = {
