e92f7260f2f0bcacd6ee2affeec13b0f41ae4069
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-07-08 11:26:48 +0300

    Better assets

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -67,8 +67,8 @@ if shared.VapeExecuted then
 		["vape/assets/WorldIcon.png"] = "rbxassetid://13350796199",
 		["vape/assets/VapeIcon.png"] = "rbxassetid://13350808582",
 		["vape/assets/RenderIcon.png"] = "rbxassetid://13350832775",
-		["vape/assets/VapeLogo1.png"] = "rbxassetid://13350860863",
-		["vape/assets/VapeLogo3.png"] = "rbxassetid://13350872035",
+		["vape/assets/VapeLogo1.png"] = "rbxassetid://18391256757",
+		["vape/assets/VapeLogo3.png"] = "rbxassetid://18391160743",
 		["vape/assets/VapeLogo2.png"] = "rbxassetid://13350876307",
 		["vape/assets/VapeLogo4.png"] = "rbxassetid://13350877564"
 	}
