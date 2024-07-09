02136bc4bbc5649d24b86158da1a54b479c726d4
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-07-09 13:02:40 +0300

    fix

--- CustomModules/6872265039.lua
+++ CustomModules/6872265039.lua
@@ -2294,7 +2294,16 @@ run(function()
 		end
 	})
 end)
-
+local function vapeGithubRequest(scripturl)
+	if not isfile("vape/"..scripturl) then
+		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/Erchobg/vapevoidware/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
+		assert(suc, res)
+		assert(res ~= "404: Not Found", res)
+		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
+		writefile("vape/"..scripturl, res)
+	end
+	return readfile("vape/"..scripturl)
+end
 local ProtectedModules
 if shared.ProtectedModules then ProtectedModules = shared.ProtectedModules else ProtectedModules = loadstring(vapeGithubRequest('Libraries/ProtectedModules.lua'))() end
 ProtectedModules.LoadModules(6872265039)
